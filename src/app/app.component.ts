17b78c23dc23d772eee98e9dfe5cded848c3f7fe
Bini M Babu <binibabu22@gmail.com>
2023-01-15 16:55:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe';
+ nameString = '';
+  employees = [{
+      firstName: 'Rohit',
+      lastName: 'Sharma',
+      dept: 'Finance',
+      salary: 5000,
+      doj: new Date('2012-04-22')
+    },
+    {
+      firstName: 'Aditi',
+      lastName: 'Mishra',
+      dept: 'Sales',
+      salary: 6000,
+      doj: new Date('2016-09-16')
+    },
+    {
+      firstName: 'Dipti',
+      lastName: 'Singh',
+      dept: 'IT',
+      salary: 10000,
+      doj: new Date('2021-09-03')
+    }
+  ]
+ 
+  addUser(){
+    this.employees.push({
+      firstName: 'Rahul',
+      lastName: 'Yadav',
+      dept: 'HR',
+      salary: 8000,
+      doj: new Date('2016-11-19')
+    })
+  }
+ 
+  reset(){
+    this.employees = this.employees.slice()
+  }
 }
